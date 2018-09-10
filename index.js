a69c10a7dba781e231866031949600bc25bcc8e3
Hank Liu <hanke.liu@gmail.com>
2018-09-10 15:44:21 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -31,7 +31,10 @@ function viewCart() {
 
   if (cart.length === 0) {
     return "Your shopping cart is empty."
-  } else {
+  } else if (cart.length === 1) {
+    return `In your cart, you have ${cart[0].itemName} at $${cart[0].itemPrice}.`
+    
+  } {
 
     for (var i=0; i < cart.length-1; i++) {
       allItemsPriceInString.concat(`${itemList[i].itemName}  at $${cart[i].itemPrice},`);
