e4a25d8fa1661a56c912f9b4aefd7f908a2a0c86
Hank Liu <hanke.liu@gmail.com>
2018-09-10 17:25:22 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -78,7 +78,8 @@ function placeOrder(cardNumber) {
      return "Sorry, we don't have a credit card on file for you."
 
   } else {
-    //cart.splice(0, cart.length); //cart = []; do not use slice, which is nodesctructive.
+    total();
+    cart.splice(0, cart.length); //cart = []; do not use slice, which is nodesctructive.
     return `Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`
   }
 
